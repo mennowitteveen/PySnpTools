77c7598bf7fa0de73b82fede66914dab67ab84c7
Carl Kadie <carlk@msn.com>
2020-05-10 07:11:11 -0700

    Fix doc tests

--- pysnptools/util/__init__.py
+++ pysnptools/util/__init__.py
@@ -55,9 +55,13 @@ def intersect_apply(data_list, sort_by_dataset=True, intersect_before_standardiz
     >>> from pysnptools.standardizer import Unit
     >>> #Create five datasets in different formats
     >>> ignore_in = None
-    >>> kernel_in = SnpKernel(Bed('../../tests/datasets/all_chr.maf0.001.N300',count_A1=False),Unit()) # Create a kernel from a Bed file
-    >>> pheno_in = Pheno('../../tests/datasets/phenSynthFrom22.23.N300.randcidorder.txt',missing="")
-    >>> cov = Pheno('../../tests/datasets/all_chr.maf0.001.covariates.N300.txt',missing="").read()
+    >>> from pysnptools.util import example_file
+    >>> bedfile = example_file("tests/datasets/all_chr.maf0.001.N300.*","*.bed")
+    >>> phenofile = example_file("tests/datasets/phenSynthFrom22.23.N300.randcidorder.txt")
+    >>> covfile = example_file("tests/datasets/all_chr.maf0.001.covariates.N300.txt")
+    >>> kernel_in = SnpKernel(Bed(bedfile,count_A1=False),Unit()) # Create a kernel from a Bed file
+    >>> pheno_in = Pheno(phenofile,missing="")
+    >>> cov = Pheno(covfile,missing="").read()
     >>> cov_as_tuple_in = (cov.val,cov.iid) #We could do cov directly, but as an example we make it a tuple.
     >>>
     >>> # Create five new datasets with consistent iids

ce69a8cb73d3b9e9293ed02ff12c885201d83d4f
Carl Kadie <carlk@msn.com>
2020-07-23 09:57:06 -0700

    Get int8 changes working with GCC

--- pysnptools/snpreader/CPlinkBedFile.h
+++ pysnptools/snpreader/CPlinkBedFile.h
@@ -2,40 +2,42 @@
 #define ORDERC
 #undef ORDERF
 #define SUFFIX(NAME) NAME ## doubleCAAA
-REAL SUFFIX(unknownOrMissing) = std::numeric_limits<REAL>::quiet_NaN();  // now used by SnpInfo
+#undef MISSING_VALUE
 #include "CPlinkBedFileT.h"
 
 #define REAL float
 #define ORDERC
 #undef ORDERF
+#undef MISSING_VALUE
 #define SUFFIX(NAME) NAME ## floatCAAA
-REAL SUFFIX(unknownOrMissing) = std::numeric_limits<REAL>::quiet_NaN();  // now used by SnpInfo
 #include "CPlinkBedFileT.h"
 
 #define REAL double
 #define ORDERF
 #undef ORDERC
+#undef MISSING_VALUE
 #define SUFFIX(NAME) NAME ## doubleFAAA
-REAL SUFFIX(unknownOrMissing) = std::numeric_limits<REAL>::quiet_NaN();  // now used by SnpInfo
 #include "CPlinkBedFileT.h"
 
 #define REAL float
 #define ORDERF
 #undef ORDERC
+#undef MISSING_VALUE
 #define SUFFIX(NAME) NAME ## floatFAAA
-REAL SUFFIX(unknownOrMissing) = std::numeric_limits<REAL>::quiet_NaN();  // now used by SnpInfo
 #include "CPlinkBedFileT.h"
 
+#undef REAL
 #define REAL signed char
 #define ORDERF
 #undef ORDERC
+#define MISSING_VALUE -127
 #define SUFFIX(NAME) NAME ## int8FAAA
-REAL SUFFIX(unknownOrMissing) = -127;  // now used by SnpInfo
 #include "CPlinkBedFileT.h"
 
+#undef REAL
 #define REAL signed char
 #define ORDERC
 #undef ORDERF
+#define MISSING_VALUE -127
 #define SUFFIX(NAME) NAME ## int8CAAA
-REAL SUFFIX(unknownOrMissing) = -127;  // now used by SnpInfo
 #include "CPlinkBedFileT.h"

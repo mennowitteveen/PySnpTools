1c53b6de9482b60b793010bbef63500a98d3f67c
Carl Kadie <carlk@msn.com>
2022-10-21 16:15:46 -0700

    update readme

--- dockers/linuxbuild/clone.sh
+++
@@ -1,6 +0,0 @@
-git pull origin
-git lfs pull
-source fresh/bin/activate
-pip install cython
-pip install -r requirements.txt
-python3 setup.py build_ext --inplace
--- dockers/linuxbuild/clone_and_test.sh
+++
@@ -1,8 +0,0 @@
-git pull origin
-git lfs pull
-source fresh/bin/activate
-pip install cython
-pip install -r requirements.cbgen102.txt
-python3 setup.py build_ext --inplace
-cd tests
-python3 test.py

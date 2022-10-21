6b7ff71062f23360811facee5719b66189fc91c0
Carl Kadie <carlk@msn.com>
2022-10-21 14:45:14 -0700

    action

--- dockers/linuxbuild/clone_and_test.sh
+++ dockers/linuxbuild/clone_and_test.sh
@@ -2,7 +2,7 @@ git pull origin
 git lfs pull
 source fresh/bin/activate
 pip install cython
-pip install -r requirements.txt
+pip install -r requirements.cbgen102.txt
 python3 setup.py build_ext --inplace
 cd tests
 python3 test.py
